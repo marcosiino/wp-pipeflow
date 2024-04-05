4c0ea6964d97ced6c66db5a12e1998785c0083c0
Marco Siino <siino.marco@gmail.com>
2024-04-06 01:50:42 +0200

    update cron time interval

--- paginedacolorare-ai.php
+++ paginedacolorare-ai.php
@@ -43,8 +43,8 @@ add_action( 'pdc_ai_cron_hook', 'cron_exec' );
 add_filter( 'cron_schedules', 'cron_interval' );
 function cron_interval( $schedules ) {
     $schedules['generate_content_interval'] = array(
-        'interval' => 60,
-        'display'  => esc_html__( 'Every 60 Seconds' ), );
+        'interval' => 3600,
+        'display'  => esc_html__( 'Every 3600 Seconds' ), );
     return $schedules;
 }
 
