a7956a5a923d6ac70ae57d19f5f90893c5559eb9
Marco Siino <siino.marco@gmail.com>
2024-04-24 17:56:07 +0200

    changed parameter name to resultTo in AIImageGenerationStageFactory too

--- classes/Pipeline/Stages/AIImageGeneration/AIImageGenerationStageFactory.php
+++ classes/Pipeline/Stages/AIImageGeneration/AIImageGenerationStageFactory.php
@@ -18,7 +18,7 @@ class AIImageGenerationStageFactory implements AbstractStageFactory
     public function instantiate(array $configuration): AbstractPipelineStage
     {
         $prompt = Helpers::getField($configuration, "prompt", true);
-        $outputParamName = Helpers::getField($configuration, "outputParamName", true);
+        $outputParamName = Helpers::getField($configuration, "resultTo", true);
         $imagesCount = Helpers::getField($configuration, "imagesCount", false, 1);
         $hdQuality = Helpers::getField($configuration, "higherQuality", false, false);
         $model = Helpers::getField($configuration, "model", false, "dall-e-2");
@@ -34,7 +34,7 @@ class AIImageGenerationStageFactory implements AbstractStageFactory
         $description = "Generates one or more images with OpenAI and outputs the generated image urls into the output context.";
         $setupParameters = array(
             "prompt" => "The image generation prompt for the AI. You can use Context Placeholders to feed context values (i.e. results from previous stages) into the prompt.",
-            "outputParamName" => "The name of the context parameter to which the generated images URLS are saved",
+            "resultTo" => "The name of the context parameter to which the generated images URLS are saved",
             "imagesCount" => "(optional, default: 1) An integer which specified the number of images to generate.",
             "higherQuality" => "(optional, default: false) A boolean which specifies if the images should be generated with higher quality. Implies higher costs.",
             "imagesSize" => "(optional, default: \"512x512\") A string specifying the size of the generated image, i.e. \"512x512\". The specified size must by supported by the selected model",
